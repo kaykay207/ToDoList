8203ef3fcbf8d213bda331c36a47fe2abc232af5
Kayla <kan100307@gmail.com>
2023-07-18 15:14:45 -0400

    Iteration 1 Complete! Able to add tasks and show important tasks.

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Scholar")
+            HStack {
+                Text("To Do List")
+                     .font(.system(size: 40))
+                     .fontWeight(.black)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.showNewTask = true
+                }) {
+                Text("+")
+                }
+                
+                Spacer()
+                
+            }
+            .padding()
+            
         }
         .padding()
+        
+        List {
+            ForEach (toDoItems) { toDoItem in
+                if toDoItem.isImportant == true {
+                    Text("‼️" + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }
+            }
+        }
+        .listStyle(.plain)
+        
+        if showNewTask {
+            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+        }
     }
 }
 
--- ToDoList/ToDoItem.swift
+++ ToDoList/ToDoItem.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class ToDoItem: Identifiable {
+    var title = ""
+    var isImportant = false
+    var id = UUID()
+    
+    init(title: String, isImportant: Bool = false) {
+            self.title = title
+            self.isImportant = isImportant
+    }
+}
